dc5257717a45fa89e14a8ed4c7e1e6a75241b85f
SrGandalff <160274176+SrGandalff@users.noreply.github.com>
2024-03-21 15:46:35 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+# importando as bibliotecas
+import pandas as pd
+...
+
+# Estrutura a base inicial do painel
+# Logos e título, e possivelmente os containers
+
+ # O banco possivelmente do google drive
