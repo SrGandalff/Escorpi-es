1e46f4b61ec18da9d8cf53c7c0e81abd1b048eeb
SrGandalff <160274176+SrGandalff@users.noreply.github.com>
2024-03-22 14:54:48 -0300

    Update and rename app.py to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,13 @@ import plotly.graph_objects as go
 import geopandas as gpd
 
 # Estrutura a base inicial do painel
+
+# Carregar os dados do Excel em um DataFrame pandas
+dados_escorpioes = pd.read_excel('caminho/do/seu/arquivo.xlsx')
+
+# Visualizar as primeiras linhas dos dados para verificar se foram carregados corretamente
+print(dados_escorpioes.head())
+
 # Logos e título, e possivelmente os containers
 
  # O banco possivelmente do google drive
