dc3f2538b05e809cac2bff14b8fd8fb9ff66579f
SrGandalff <160274176+SrGandalff@users.noreply.github.com>
2024-03-22 14:34:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 # importando as bibliotecas
 import pandas as pd
-...
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
+import geopandas as gpd
 
 # Estrutura a base inicial do painel
 # Logos e título, e possivelmente os containers
